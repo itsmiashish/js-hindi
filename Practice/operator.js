b6c71be6d4a2175e6d3b12fcb55e445ec215a864
itsmiashish <125015707+itsmiashish@users.noreply.github.com>
2024-09-22 03:26:39 +0000

    some operator

---
+++ Practice/operator.js
@@ -0,0 +1,18 @@
+// operator mathetical operator and compression operator.
+console.log(5+6);
+console.log(5-2);
+console.log(5*9);
+console.log(8/2);
+console.log(9%2);
+
+// these are mathetical operator.
+
+console.log(5>2);
+console.log(55<8);
+console.log('2'==2);
+console.log('2'===2);
+
+// these is compression operator.
+
+
+
