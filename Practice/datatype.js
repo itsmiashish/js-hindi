6dc01dde78d802a06eb9cf70f10862254b3289df
itsmiashish <125015707+itsmiashish@users.noreply.github.com>
2024-09-22 03:01:27 +0000

    write some datatype.

---
+++ Practice/datatype.js
@@ -0,0 +1,56 @@
+// let  str = "ashish"; // string datatype;
+// console.log(str);
+// let number =56;  // number datatype;
+// console.log(number);
+
+//  datatype(these all datatype);
+//string; 
+//number;
+//boolean;
+//symbol;
+//undefine;
+//null;
+//object;
+
+
+let value;
+console.log(value);
+let value2 =null;
+console.log(value2);
+console.log(typeof(value2));
+console.log(typeof(value));
+// here we see when we declare value then not assign value then we see there type is undefine.
+// and if we assign null there type is object. 
+
+let num = 88;
+let SomeString= String(num);
+console.log(SomeString);
+console.log(typeof(SomeString));
+let name ="ashish";
+let RandomNumber = Number(name);
+console.log(RandomNumber);
+console.log(typeof(RandomNumber));
+let FirstName ="Ashish";
+let LastName ="kumar";
+let FullName = FirstName+LastName;
+console.log(FullName);
+
+//here we see how convert number to string & string to number.
+// when we convert string to number then we use Captial letter N. And its give type is number but actual value print NaN.
+//Add twwo string. Not subtract😂.
+
+
+let person ={
+Name:"ashish",
+age:56
+}
+ // these is object datatype. 
+
+console.log(person);
+console.log(typeof(person));
+
+
+
+
+
+
