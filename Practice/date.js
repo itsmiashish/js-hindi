18e5c53940a9090e78223565d48a8c9a08ba20d9
itsmiashish <125015707+itsmiashish@users.noreply.github.com>
2024-09-24 14:09:28 +0000

    practices on date and time

---
+++ Practice/date.js
@@ -0,0 +1,32 @@
+// let todayDate = new Date();
+// console.log(`toStriing ${todayDate.toString()}`);
+// console.log(`toDateSting ${todayDate.toDateString()}`);
+// console.log(` to JSON ${ todayDate.toJSON()}`);
+// console.log(` toLocalString ${todayDate.toLocaleString()} `);
+// date type is object.
+
+// let myDate = new Date(2024, 9,24);
+// console.log(myDate.toLocaleString());
+
+// let nowDate = Date.now();
+// console.log(nowDate.toLocaleString());
+// let ourDate = new Date();
+// console.log(ourDate.getDate());
+// console.log(ourDate.getDay());
+// console.log(ourDate.getMonth());
+
+let newDate = new Date();
+newDate.toLocaleString('default',{
+    weekday:"long"
+})
+console.log(newDate);
+
+
+
+
+
+
+
+
+
+
