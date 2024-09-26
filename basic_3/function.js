744f8a3dc769a6bc0b013ddfb520132007c73888
itsmiashish <125015707+itsmiashish@users.noreply.github.com>
2024-09-26 12:28:24 +0000

    function code

---
+++ basic_3/function.js
@@ -0,0 +1,18 @@
+function hello() {
+    console.log("Hello ashish");
+    
+}
+hello();
+
+function sum(number1, number2) {
+    console.log(number1+number2);
+    
+}
+sum(7,8.8);
+
+function multiplyNumber(num1,num2) {
+    return num1+num2;
+    
+}
+const result = multiplyNumber(7,9);
+console.log(result);
