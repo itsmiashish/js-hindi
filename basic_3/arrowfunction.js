df0e83fef62b9cb11ecbb26cc3d6dc31ee870fa2
itsmiashish <125015707+itsmiashish@users.noreply.github.com>
2024-09-29 14:50:11 +0000

    some line of code arrowfunction

---
+++ basic_3/arrowfunction.js
@@ -0,0 +1,31 @@
+// arrow function is funtion, it's only way to write diff type write function when come es6 then introduce these function.
+
+function hello() {
+    console.log('ashish kumar sharma ');
+    
+}
+hello()// this is normal function.
+
+const hello1 = ()=>{
+console.log("welcome Ashish Babu");
+
+}
+hello1() // this is Arrow funtion.
+
+ const sum =()=>{
+    let a =66;
+    let b = 66;
+    console.log(a*b);
+}
+sum();
+
+const sub = ( num1,num2)=> num1-num2
+console.log(sub(66,6)); // you write this type also  arrow funtion. 
+
+const multi = (n,n1) => (n*n1)
+console.log(multi(8,8));// when you this type arrow funtion and you use  parentheses than you not write return keyword.
+ 
+const div = (num1, num2)=>{
+    return num1/num2;
+}
+console.log(div(66,2));// when you write curly brackets then you write return keyword. 
