cd6aadae69554a150652a5836ee2bfd713dc5971
itsmiashish <125015707+itsmiashish@users.noreply.github.com>
2024-09-29 15:06:31 +0000

    simple disscusion on life

---
+++ basic_3/life.js
@@ -0,0 +1,16 @@
+// Immediately Invoked Function Expressions (IIFE)
+//life stand for  Immdedidaitely Invoked Function  Expression 
+
+(function cahi() {
+    console.log("welcome ashish ji");
+    
+})(); // this is life.
+(()=>{
+    console.log("sher ki dhahar to jante ho na rana ");
+    
+})()
+( function Rana() {
+  console.log('dk Botiya nochne wala  gidar kabhi sher nhi ban skta hai');
+    })()
+
+    //  life means when you wnat to excute any function immdedidaitely then use this syntax ()
