cf17ed88c3aa09f2d825f2772cfd94944388930b
itsmiashish <125015707+itsmiashish@users.noreply.github.com>
2024-10-03 10:53:42 +0000

    forloop

---
+++ loop/forloop.js
@@ -0,0 +1,20 @@
+// lop means in single line code print multiple line.
+for (let i = 0; i <=10; i++) {
+    // console.log(i*5);
+}
+for(let i =1; i<=10;i++)
+{
+    for(let j=1;j<=10;j++)
+    {
+        console.log(i*j);
+     
+    }
+    console.log();
+    
+}
+
+console.log("Print tabel one to 10");
+
+// print 25,20,15,10,5,0;
+
+for(let i =)
