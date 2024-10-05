8098b96627a4a61b61ca39d3472b436b33d12409
itsmiashish <125015707+itsmiashish@users.noreply.github.com>
2024-10-05 14:35:23 +0000

    filter line add

--- loop/objectloop.js
+++ loop/objectloop.js
@@ -23,6 +23,20 @@ for (const value in subject) {
 
 // })
 
+const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumers.map( (num) => { return num + 10})
+
+const newNums = myNumers
+                .map((num) => num * 10 )
+                .map( (num) => num + 1)
+                .filter( (num) => num >= 40)
+
+console.log(newNums);
+
+
+
+
 
 
 const newArr = [
@@ -43,13 +57,18 @@ const newArr = [
   }];
 
 //   console.log(newArr);
-newArr.forEach((item)=>{
-    // console.log(item.name,item.price);
-    console.log(item.price);
+// newArr.forEach((item)=>{
+//     // console.log(item.name,item.price);
+//     console.log(item.price);
     
-})  
+// })  
 
-// filter => when you use filter then you pass a condition then they work.
+newArr.map((item)=>console.log(item.name+"="+ item.price)
+)
+
+
+
+// filter => when you use filter then you pass a condition then they work. it,s filter value according to your choice.
 
 const num =[1,5,4,7,8,9,4,3,56];
 const result = num.filter((item)=>{
@@ -57,3 +76,31 @@ const result = num.filter((item)=>{
 })
 console.log(result);
 
+const books = [
+  { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+  { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+  { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+  { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+  { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+  { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+  { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+  { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+  { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+];
+// const Books = books.map((item)=>{
+//   // console.log(item);
+//   // return item;
+  
+// })
+// console.log(Books);
+
+// let result1 = books.filter((item)=>{
+// return item.publish>2000;
+// })
+
+// console.log(result1);
+
+const Books = books.filter((item)=>{
+ return item.edition >1995 && item.publish>1995
+})
+console.log(Books);
